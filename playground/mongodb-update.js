917d8f8641ffa361ac11562cee813c3003d7e592
Sean Fallmann <sfallmann@pbmbrands.com>
2017-01-25 15:53:55 -0500

    status

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -9,16 +9,15 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db)=>{
     }
     console.log('Connected to mongo db');
 
-    db.collection('Todos').findOneAndUpdate({ 
-        _id: "5888f5f3671af93c94f6418c" 
+    db.collection('Users').findOneAndUpdate({ 
+        _id: new ObjectID("5888c2ae7067d904205b34bd")
     }, { 
-        $set: { completed: true }
+        $inc: { age: -1 },
     }, {
         returnOriginal: false
     }).then((result) => {
         console.log(result);
     })
-
     db.close();
 
 
