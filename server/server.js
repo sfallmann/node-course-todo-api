09f9c34d7ee89509a4d88cad0d37f8b66b2caa71
Sean Fallmann <jorundr@hotmail.com>
2017-01-26 21:20:45 -0500

    Added /GET todos

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ var port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-app.post('/todos',(req, res, next) => {
+app.post('/todos', (req, res, next) => {
     var todo = new Todo({
         text: req.body.text
     });
@@ -18,10 +18,29 @@ app.post('/todos',(req, res, next) => {
     todo.save().then((doc) => {
         console.log(doc);
         res.send(doc);
-    }, (e) => {
+    }).catch((err) => {
         res.status(400).send(e);
     });
 });
+
+app.get('/todos', (req, res, next) => {
+    Todo.find().then((todos) => {
+        console.log(todos);
+        res.send({todos});
+    }).catch((err) => {
+        res.status(400).send(err);
+    });
+})
+
+app.get('/todos/:id', (req, res, next) => {
+    Todo.find({ _id: id }).then((todo) => {
+        console.log(todo);
+        res.send({todo});
+    }).catch((err) => {
+        res.status(400).send(err);
+    });
+})
+
 
 app.listen(port, () => {
     console.log('Started on port ', port);
