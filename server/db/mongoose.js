dd7bbbc4c276e9b8fded8ecc726ddfca127cd809
Sean Fallmann <jorundr@hotmail.com>
2017-01-27 22:16:23 -0500

    fixed typo

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://"test":"test"@ds056009.mlab.com:56009/node-todo-sample');
+mongoose.connect('mongodb://test:test@ds056009.mlab.com:56009/node-todo-sample');
 
 module.exports = { mongoose };
