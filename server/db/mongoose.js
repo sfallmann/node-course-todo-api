333cf1a99e9e4f63923a7afce7d0da65f009b2d5
Sean Fallmann <jorundr@hotmail.com>
2017-01-28 14:54:31 -0500

    Setup seperate test and dev dbs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://test:test@ds056009.mlab.com:56009/node-todo-sample');
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = { mongoose };
